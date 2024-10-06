bebcbae168ca99c250291c2456e2c9dd49038f28
shakir-fattani <m.shakirfattani@gmail.com>
2024-10-06 22:30:36 +0400

    adding package-lock.json

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 import Router from './router.js'
 
 const apiRouter = new Router();
-apiRouter.get("/health", ({ request, env, ctx }) => new Response("success"));
+apiRouter.get("/health", ({ request, env, ctx }) => new Response("200 success", { status: 200 }));
 apiRouter.get("/api/todos", ({ request, env, ctx }) => new Response("Todos Index!"));
 apiRouter.get("/api/todos/:id", ({ params, request, env, ctx }) => new Response(`Todo #${params.id}`));
 
